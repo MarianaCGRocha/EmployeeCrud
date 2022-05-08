2b09751912f5075122e3bfea7bde6e7794cb2356
Mariana Rocha <marianagomesr@gmail.com>
2022-05-08 17:18:13 -0300

    Implements connection and save method

--- src/Employee.java
+++ src/Employee.java
@@ -1,6 +1,10 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Employee {
     private JPanel Main;
@@ -22,12 +26,53 @@ public class Employee {
         frame.setVisible(true);
     }
 
+    Connection con;
+    PreparedStatement pst;
 
+    public void connect() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/mgcompany", "root", "");
+            System.out.println("Sucess");
+        } catch(ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
 
     public Employee() {
+        connect();
         saveButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
+                String employeeSalary, employeePhone, employeeName;
+
+                employeeName = textName.getText();
+                employeeSalary = textSalary.getText();
+                employeePhone = textPhone.getText();
+
+                try {
+                    pst = con.prepareStatement("insert into employee(employeeName, employeeSalary, employeePhone) " +
+                            "values(?, " +
+                            "?, ?)");
+                    pst.setString(1, employeeName);
+                    pst.setString(2, employeeSalary);
+                    pst.setString(3, employeePhone);
+                    pst.executeUpdate();
+                    JOptionPane.showMessageDialog(null, "Record Added!");
+                    // table_load();
+                    textName.setText("");
+                    textSalary.setText("");
+                    textPhone.setText("");
+                    textName.requestFocus();
+                } catch (SQLException el) {
+                    el.printStackTrace();
+                }
+
+
+
 
             }
         });
